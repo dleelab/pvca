2e3cfe3082fbe558e5c3cae3747bb01d0c558055
dleelab <dustorm@gmail.com>
2017-02-13 14:16:50 -0500

    fist commit

---
+++ R/plotpvca.R
@@ -0,0 +1,32 @@
+#' A function for plotting a result from the PVCA function
+#'
+#' This function takes as input a proportion vector calculated from the 'pvca' function and generate a bar plot.
+#'
+#' @param pvca.res The output from the 'pvca' function
+#' @param title  The title of the bar plot
+#'
+#' @return p A ggplot2 object
+#'
+#' @export
+#'
+
+PlotPVCA <- function(pvca.res, title){
+  plot.dat <- data.frame(eff=names(pvca.res), prop=pvca.res)
+  p <- ggplot(plot.dat, aes(x=eff, y=prop))
+  p <- p + ggtitle(title)
+  p <- p + geom_bar(stat="identity", fill="steelblue", colour="steelblue")
+  p <- p + geom_text(aes(label=round(prop,3), y=prop+0.04), size=4)
+  p <- p + scale_x_discrete(limits=names(pvca.res))
+  p <- p + scale_y_continuous(limits = c(0,1))
+  p <- p + labs(x= "Effects", y= "Weighted average proportion variance")
+  p <- p + theme_bw()
+  p <- p + theme(plot.background = element_blank() ,panel.grid.major = element_blank(),
+                 panel.grid.minor = element_blank() ,panel.border = element_blank(), panel.background = element_blank())
+  p <- p + theme(axis.line = element_line(color = 'black'))
+  p <- p + theme(axis.title.x = element_text(size = 15, vjust=-0.5))
+  p <- p + theme(axis.title.y = element_text(size = 15, vjust= 1.0))
+  p <- p + theme(axis.text = element_text(size = 12))
+  p <- p + theme(axis.text.x = element_text(angle = 90, vjust= 0.5, hjust=1))
+  p
+}
+
---
+++ README.md
@@ -0,0 +1,24 @@
+PVCA (Principal Variance Component Analysis) for sequencing read counts
+-----------------------------------------------------------------------
+
+Author: Donghyung Lee
+
+Note: The function is written based on the 'pvcaBatchAssess' function of the PVCA R package and slightly changed to make it more efficient and flexible. (<http://watson.nci.nih.gov/bioc_mirror/packages/release/bioc/manuals/pvca/man/pvca.pdf>)
+
+Description: Function for Principal Variance Component Analysis
+
+Release Date: 04/18/2016
+
+Input:
+
+      1. counts: normalized(e.g. TMM) or log-transformed reads count matrix from sequencing data (row:gene/feature, col:sample) 
+               
+      2. meta: meta data matrix containing predictor variables (row:sample, col:predictor)
+      
+      3. threshold: proportion of the variation in read counts explained by k top PCs. This value determines the number of PCs to be used in pvca. 
+      
+      4. inter: TRUE/FALSE - include/do not include pairwise interactions of predictors
+
+Output:
+
+       a vector of proportions of the variation in read counts data explained by each predictor.
